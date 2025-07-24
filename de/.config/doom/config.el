7eeb4f7adc7907b6b641f6a7334052c092251fdf
alfordm999 <108696717+alfordm999@users.noreply.github.com>
2025-07-23 22:40:22 -0400

    typo

--- de/.config/doom/config.el
+++ de/.config/doom/config.el
@@ -65,11 +65,7 @@
 ;;   this file. Emacs searches the `load-path' when you load packages with
 ;;   `require' or `use-package'.
 ;; - `map!' for binding new keys
-;;
-(map! :leader
-      (:prefix ("o" . "open")
-       :desc "Open vterm terminal"
-       "t" (lambda () (interactive) (term "vterm"))))
+
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
