1ece48226c148b165485e7c81adb4ca059facf73
alfordm999 <108696717+alfordm999@users.noreply.github.com>
2025-09-04 10:14:23 -0400

    kali tools added and EQ off mode

--- de/.local/bin/br0.sh
+++ de/.local/bin/br0.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo ip link add name br0 type bridge
 sudo ip link set  br0 up
-sudo ip link set enp5s0 master br0
+sudo ip link set enp6s0 master br0
 sudo ip addr add 192.168.1.10/24 dev br0
