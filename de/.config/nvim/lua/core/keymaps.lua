aaf4ce0abd217d32a071f0d5b1baca019a7e7767
Miles Alford <alfordm999@gmail.com>
2025-09-05 18:13:12 -0400

    nvim: add open to file

--- de/.config/nvim/lua/core/keymaps.lua
+++ de/.config/nvim/lua/core/keymaps.lua
@@ -21,3 +21,14 @@ vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
+vim.keymap.set("n", "gx", function()
+  local path = vim.fn.expand("<cfile>")  -- word under cursor
+  if vim.fn.filereadable(path) == 1 then
+    vim.cmd("edit " .. path)             -- open file in current window
+  elseif vim.fn.isdirectory(path) == 1 then
+    vim.cmd("Explore " .. path)          -- open dir in netrw (or nvim-tree if you use it)
+  else
+    print("Not a file: " .. path)
+  end
+end, { silent = true, noremap = true, desc = "Open file under cursor" })
+
