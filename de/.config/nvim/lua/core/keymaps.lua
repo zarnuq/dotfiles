b77b40e8464ae1150504f366774fe5b2741047af
alfordm999 <108696717+alfordm999@users.noreply.github.com>
2025-09-07 21:17:19 -0400

    nvim: larger tabs

--- de/.config/nvim/lua/core/keymaps.lua
+++ de/.config/nvim/lua/core/keymaps.lua
@@ -16,8 +16,8 @@ vim.opt.cursorline = true
 vim.opt.autoread = true
 vim.opt.clipboard = "unnamedplus"
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
