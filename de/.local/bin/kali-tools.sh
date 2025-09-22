7d5b1cd171f3aeb7c4345785bb079a9286b48c51
alfordm999 <108696717+alfordm999@users.noreply.github.com>
2025-09-22 08:43:26 -0400

    kalitoolsupdate + tmux resurect

---
+++ de/.local/bin/kali-tools.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sudo paru -S        \
+metasploit          \
+netexec             \
+enum4linux          \
+ffuf                \
+ghidra              \
+wireshark-qt        \
+pngcheck            \
+smblcient           \
+openssh             \
+impacket            \
+dirbuster           \
+dnslookup-bin       \
+
+
