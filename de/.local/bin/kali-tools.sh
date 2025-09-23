4c7e3cf7f150b9db584c9d58f0fde27d66ac4196
alfordm999 <108696717+alfordm999@users.noreply.github.com>
2025-09-23 12:21:58 -0400

    kalitoolsupdate + tmux resurect

--- de/.local/bin/kali-tools.sh
+++ de/.local/bin/kali-tools.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-sudo paru -S        \
-metasploit          \
-netexec             \
-enum4linux          \
-ffuf                \
-ghidra              \
-wireshark-qt        \
-pngcheck            \
-smblcient           \
-openssh             \
-impacket            \
-dirbuster           \
-dnslookup-bin       \
-
+sudo paru -S    \   
+metasploit      \ # Popular penetration testing framework for exploit development and scanning 
+netexec         \ # Swiss army knife for pentesting Active Directory and network protocols 
+enum4linux      \ # Tool for gathering information from Windows machines via SMB 
+ffuf            \ # Fast web fuzzer for discovering files, directories, parameters 
+ghidra          \ # NSA’s open-source reverse engineering suite 
+wireshark-qt    \ # GUI network protocol analyzer for capturing and inspecting packets 
+pngcheck        \ # Verifies PNG image integrity and displays info about PNG files 
+smbclient       \ # Command-line SMB/CIFS client for accessing Windows/Samba shares 
+openssh         \ # SSH client and server for secure remote login and file transfer 
+impacket        \ # Python collection of tools for working with network protocols (SMB, RDP, etc.) 
+dirbuster       \ # Directory and file brute-forcing tool for web servers 
+dnslookup-bin   \ # DNS query utility (nslookup-like functionality) 
+remmina         \ # Remote desktop client supporting RDP, VNC, SSH, SPICE 
+freerdp         \ # Free implementation of the Remote Desktop Protocol (RDP) 
 
