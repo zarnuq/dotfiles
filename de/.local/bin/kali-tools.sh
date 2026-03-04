aaed8cc8f401c417998d82ba65fec4cf7183a9f7
alfordm999 <108696717+alfordm999@users.noreply.github.com>
2026-03-04 15:30:44 -0500

    kali tools add stuff

--- de/.local/bin/kali-tools.sh
+++ de/.local/bin/kali-tools.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
-sudo paru -S    \   
-metasploit      \ # Popular penetration testing framework for exploit development and scanning 
-netexec         \ # Swiss army knife for pentesting Active Directory and network protocols 
-enum4linux      \ # Tool for gathering information from Windows machines via SMB 
-ffuf            \ # Fast web fuzzer for discovering files, directories, parameters 
-ghidra          \ # NSA’s open-source reverse engineering suite 
-wireshark-qt    \ # GUI network protocol analyzer for capturing and inspecting packets 
-pngcheck        \ # Verifies PNG image integrity and displays info about PNG files 
-smbclient       \ # Command-line SMB/CIFS client for accessing Windows/Samba shares 
-openssh         \ # SSH client and server for secure remote login and file transfer 
-impacket        \ # Python collection of tools for working with network protocols (SMB, RDP, etc.) 
-dirbuster       \ # Directory and file brute-forcing tool for web servers 
-dnslookup-bin   \ # DNS query utility (nslookup-like functionality) 
-remmina         \ # Remote desktop client supporting RDP, VNC, SSH, SPICE 
-freerdp         \ # Free implementation of the Remote Desktop Protocol (RDP) 
-hashcat         \ # Another Password Cracker
-john            \ # Another Password Cracker
-gobuster        \ # Enumerator for websites
+sudo paru -S        \ 
+metasploit          \ # Popular penetration testing framework for exploit development and scanning 
+netexec             \ # Swiss army knife for pentesting Active Directory and network protocols 
+enum4linux          \ # Tool for gathering information from Windows machines via SMB 
+ffuf                \ # Fast web fuzzer for discovering files, directories, parameters 
+ghidra              \ # NSA’s open-source reverse engineering suite 
+binaryninja-free    \ 
+wireshark-qt        \ # GUI network protocol analyzer for capturing and inspecting packets 
+smbclient           \ # Command-line SMB/CIFS client for accessing Windows/Samba shares 
+openssh             \ # SSH client and server for secure remote login and file transfer 
+freerdp             \ # Free implementation of the Remote Desktop Protocol (RDP) 
+impacket            \ # Python collection of tools for working with network protocols (SMB, RDP, etc.) 
+dirbuster           \ # Directory and file brute-forcing tool for web servers 
+dnslookup-bin       \ # DNS query utility (nslookup-like functionality) 
+remmina             \ # Remote desktop client supporting RDP, VNC, SSH, SPICE 
+hashcat             \ # Another Password Cracker
+john                \ # Another Password Cracker
+gobuster            \ # Enumerator for websites
+binwalk             \ 
+pngcheck            \ # Verifies PNG image integrity and displays info about PNG files 
+zsteg               \ 
+evil-winrm          \ 
 seclists
+
+
 
