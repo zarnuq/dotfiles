e6769dbff4ef38067d09c94d7f5a1b1087c9ac14
Miles <zarnuq@github.com>
2026-06-24 09:58:36 -0400

    del dwl

--- de/.local/src/someblocks/blocks/mic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-volume=$(pactl get-source-volume @DEFAULT_SOURCE@ | grep -o '[0-9]\+%' | head -1)
-muted=$(pactl get-source-mute @DEFAULT_SOURCE@ | awk '{print $2}')
-
-if [[ "$muted" == "yes" ]]; then
-    echo "${volume:-N/A}"
-else
-    echo "^fg(f38ba8)${volume:-N/A}^fg()"
-fi
